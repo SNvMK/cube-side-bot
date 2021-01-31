f5eaa05eebd3da4c73ccb5cc12f6b4e7f6452e87
SNVMK <sidzahroman@gmail.com>
2021-01-31 14:54:14 +0700

    Update main.py

--- cube-side-bot/main.py
+++ cube-side-bot/main.py
@@ -199,13 +199,14 @@ async def rcon_start(ctx):
         rcon = mcrcon.MCRcon("95.216.62.180", "41097498AA47969E1B", port=28582)
         rcon.connect()
         while True:
+            print("Сессия RCON запущена. Вводите команды, только без `/`!")
             msg = await client.wait_for("message", check=check)
             if msg.content != "exit":
                 cmd = rcon.command(msg.content)
                 await ctx.send(cmd, hidden=True)
 
             else:
-                rcon.destroy()
+                rcon.disconnect()
                 break
 
     else:
