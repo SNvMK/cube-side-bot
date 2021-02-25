d443ae8e1147e65b4007d2d670fdc36358c75ad6
SNVMK <sidzahroman@gmail.com>
2021-02-25 20:51:42 +0700

    a

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bot = new dbd.Bot({
 bot.status({
     type: "STREAMING",
     text: "летсплей",
+    time: 12,
     url: "https://vk.com/csides"
 })
 
@@ -22,6 +23,11 @@ bot.interactionCommand({
         $interactionReply[{title:«$message»}{description:$randomText[Бесспорно;Предрешено;Никаких сомнений;Определённо да;Можешь быть уверен в этом;Мне кажется — «да»;Вероятнее всего;Хорошие перспективы;Знаки говорят — «да»;Да;Пока не ясно, попробуй снова;Спроси позже;Лучше не рассказывать;Сейчас нельзя предсказать;Сконцентрируйся и спроси опять;Даже не думай;Мой ответ — «нет»;По моим данным — «нет»;Перспективы не очень хорошие;Весьма сомнительно]}{color:RED}];
     `
 })
-
-
 bot.onInteractionCreate()
+
+
+bot.joinedCommand({
+    channel: "804650873074483200",
+    code: `$sendWebhook[804724673677623326;Hnby6pAeIAX4qXJ3UPApsOfOobcdEjDd9XsceD7xnxPN2miGZFa0axMCGlhC3TCX4r8n;Зашёл на сервер;{username:$username};{avatar:$authorAvatar}]`
+})
+bot.onJoined()
