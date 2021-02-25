1a4e1b4bd98c9709f51422c62ceb0b7b126e6c48
SNVMK <sidzahroman@gmail.com>
2021-02-25 20:59:18 +0700

    a

--- index.js
+++ index.js
@@ -11,18 +11,26 @@ bot.status({
     url: "https://vk.com/csides"
 })
 
+bot.command({
+    name: "add_eval",
+    code: `$createSlashCommand[$guildID;запуск;Запуск кода(только для SNVMK);код:Код для запуска:true:3]`
+})
 
 bot.interactionCommand({
     name: "пинг",
     code: `$interactionReply[{title:🏓Понг!}{description:Пинг сервера $ping мс}{color:RED}]`
 })
-
 bot.interactionCommand({
     name: "шар",
     code: `
         $interactionReply[{title:«$message»}{description:$randomText[Бесспорно;Предрешено;Никаких сомнений;Определённо да;Можешь быть уверен в этом;Мне кажется — «да»;Вероятнее всего;Хорошие перспективы;Знаки говорят — «да»;Да;Пока не ясно, попробуй снова;Спроси позже;Лучше не рассказывать;Сейчас нельзя предсказать;Сконцентрируйся и спроси опять;Даже не думай;Мой ответ — «нет»;По моим данным — «нет»;Перспективы не очень хорошие;Весьма сомнительно]}{color:RED}];
     `
 })
+bot.interactionCommand({
+    name: "запуск",
+    code: `$interactionReply[{title:Вывод}{description:$eval[$message;yes]}{color:RED}]`
+})
+
 bot.onInteractionCreate()
 
 
@@ -30,4 +38,5 @@ bot.joinCommand({
     channel: "804650873074483200",
     code: `$sendWebhook[804724673677623326;Hnby6pAeIAX4qXJ3UPApsOfOobcdEjDd9XsceD7xnxPN2miGZFa0axMCGlhC3TCX4r8n;Зашёл на сервер;{username:$username};{avatar:$authorAvatar}]`
 })
+
 bot.onJoined()
