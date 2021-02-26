012353c73779d071590091b92a140c727a6c0596
SNVMK <sidzahroman@gmail.com>
2021-02-26 14:05:01 +0700

    a

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ bot.interactionCommand({
 bot.interactionCommand({
     name: "новость",
     code: `
+        $messagePublish[804673463469604865;$botLastMessageID]
         $useChannel[804673463469604865]
         $title[Новость]
         $description[$message]
         $color[#7289DA]
-        $messagePublish[804673463469604865;$botLastMessageID]
         $onlyPerms[admin;$dm Вы не имеете прав админа!]
     `
 });
